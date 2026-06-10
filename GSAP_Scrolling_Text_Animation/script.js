f2bc802ee0a90c8da4fe694f713b4e16ac1a67d4
HafizEngineerMuhammadAbdullah <abdullahkhalid2k25@gmail.com>
2026-06-09 21:50:18 -0700

    GSAP Scrolling Text Animation

--- GSAP_Scrolling_Text_Animation/script.js
+++ GSAP_Scrolling_Text_Animation/script.js
@@ -1,3 +1,28 @@
-window.addEventListener('wheel', () => {
-    console.log("Heyyyyyyyyyy!");
-})
+const scrollAnimation = () => {
+    window.addEventListener('wheel', (e) => {
+        console.log(e.deltaY);
+        if (e.deltaY > 0) {
+            gsap.to(".marque", {
+                transform: "translateX(-200%)",
+                duration: 2,
+                repeat: -1,
+                ease: "none"
+            });
+            gsap.to(".marque img", {
+                rotate: 180
+            });
+        } else {
+            gsap.to(".marque", {
+                transform: "translateX(0%)",
+                duration: 2,
+                repeat: -1,
+                ease: "none"
+            })
+            gsap.to(".marque img", {
+                rotate: 0
+            });
+        }
+    });
+}
+
+scrollAnimation();

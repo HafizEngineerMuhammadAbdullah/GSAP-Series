0b2777d1ec8d37522cc0f1168595a22a69adc3f7
HafizEngineerMuhammadAbdullah <abdullahkhalid2k25@gmail.com>
2026-06-17 12:52:45 -0700

    add animation

---
+++ GSAP_Animated_Website/script.js
@@ -0,0 +1,12 @@
+gsap.from("nav h1",{
+    opacity: 0,
+    y: -100,
+    duration: 1,
+    delay: 1
+})
+gsap.from("nav h4, nav button",{
+    opacity: 0,
+    y: -100,
+    duration: 1,
+    delay: 1
+})
